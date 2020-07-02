2d2906c83422f69c2dc4c4c9580693f459875cd8
serhan <serhanyavuz5@gmail.com>
2020-07-02 19:33:58 +0300

    added random DTED generator

---
+++ DTEDGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hs
+{
+    public static class DTEDGenerator
+    {
+        public static int[,] Generate()
+        {
+            Console.WriteLine("Generating DTED");
+            int[,] data = new int[401,401];
+            Random random = new Random();
+            for (int i = 0; i< 401 ; i++)
+            {
+                for (int j = 0; j< 401; j++)
+                {
+                    data[i, j] = random.Next(256);
+                }
+
+            }
+            Console.WriteLine("Generated");
+            return data;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace hs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            //Generating DTED data
+            int [,] randomGeneratedDTED = DTEDGenerator.Generate();
+            
+
+            //Visualizing
+            Bitmap bitmap;
+            unsafe
+            {
+                fixed(int* intPtr = &randomGeneratedDTED[0, 0])
+                {
+                    
+                    bitmap = new Bitmap(401, 401, 401 * 4, System.Drawing.Imaging.PixelFormat.Format32bppArgb, new IntPtr(intPtr));
+                    bitmap.Save("test", ImageFormat.Jpeg);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
